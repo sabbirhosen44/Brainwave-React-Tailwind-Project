005d613434113f69c7548709675ce1aa28743f66
Jawwad926 <mdsabbirhosen926@gmail.com>
2024-06-25 23:01:38 +0600

    Button component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import Button from "./components/Button";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button>Something</Button>
+        <ButtonGradient />
+      </div>
+    </>
+  );
+};
 
 export default App;
