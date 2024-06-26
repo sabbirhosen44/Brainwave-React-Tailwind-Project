40415a8cae2525cf8e27dccbfc9752c622170fa2
Jawwad926 <mdsabbirhosen926@gmail.com>
2024-06-26 21:07:09 +0600

    update Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { brainwave } from "../assets";
+import { navigation } from "../constants/index";
 
 const Header = () => {
   return (
     <header
       className={`fixed top-0 left-0 w-full z-50 border-b border-b-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm`}
     >
-      <div
-        className={
-          "flex items-center justify-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4"
-        }
-      >
+      <div className={"flex items-center  px-5 lg:px-7.5 xl:px-10 max-lg:py-4"}>
         <a className={"block w-[12rem] xl:mr-8 "} href="#hero">
           <img src={brainwave} width={190} height={40} alt="brainwave" />
         </a>
-        <nav></nav>
+        <nav
+          className={
+            "fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto  lg:bg-transparent"
+          }
+        >
+          <ul className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+            {navigation.map((item) => (
+              <a
+                key={item.id}
+                href={item.url}
+                className={` block relative font-code text-2xl  uppercase text-n-1 transition-colors hover:text-color-1  ${
+                  item.onlyMobile ? "lg:hidden" : ""
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold `}
+              >
+                {item.title}
+              </a>
+            ))}
+          </ul>
+        </nav>
       </div>
     </header>
   );
