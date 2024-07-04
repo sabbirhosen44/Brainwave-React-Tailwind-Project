c6026ef48b0bfb66dd1bb2e9a2ab315106492a81
Jawwad926 <mdsabbirhosen926@gmail.com>
2024-07-04 20:28:57 +0600

    completed the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,40 @@
 import React, { useRef } from "react";
 import { ScrollParallax } from "react-just-parallax";
+import { heroBackground } from "../assets";
 import curve from "../assets/hero/curve.png";
 import robot from "../assets/hero/robot.jpg";
 import { heroIcons } from "../constants/index";
 import Button from "./Button";
+import CompanyLogos from "./CompanyLogos";
 import Generating from "./Generating";
 import Notification from "./Notification";
 import Section from "./Section";
 import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 
 const Hero = () => {
-  const scrollParallaxRef = useRef(null);
+  const parallaxRef = useRef(null);
+
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
-      id="#hero"
+      customPaddings
+      id="hero"
     >
-      <div className="container relative" ref={scrollParallaxRef}>
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center  mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
             <span className="inline-block relative">
               Brainwave{" "}
-              <img src={curve} className="absolute left-0 top-full xl:-mt-2" />
+              <img
+                src={curve}
+                className="absolute left-0 top-full w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="Curve"
+              />
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
@@ -36,7 +46,7 @@ const Hero = () => {
           </Button>
         </div>
         <div className="relative max-w-[23rem]  mx-auto md:max-w-5xl xl:mb-24">
-          <div className="relative z-1 p-0 5 rounded-2xl bg-conic-gradient">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
               <div className="overflow-hidden aspect-[33/40] rounded-b-[0.9rem] md:aspect-[688/490] lg:aspect-[1024/490]">
@@ -67,8 +77,20 @@ const Hero = () => {
             </div>
             <Gradient />
           </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              className="w-full"
+              width={1440}
+              height={1800}
+              alt="hero"
+            />
+          </div>
+          <BackgroundCircles />
         </div>
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
+      <BottomLine />
     </Section>
   );
 };
