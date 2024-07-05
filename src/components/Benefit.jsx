606dbe373d1a50125f1107376408d2972b1d70b2
Jawwad926 <mdsabbirhosen926@gmail.com>
2024-07-05 19:38:09 +0600

    working on heading component

---
+++ src/components/Benefit.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Section from "./Section";
+
+const Benefit = () => {
+  return (
+    <Section id="#features">
+      <div
+        className="container relative z-2
+      "
+      >
+        <Heading className="" title="Chat Smarter, Not Harder with Brainwave" />
+      </div>
+    </Section>
+  );
+};
+
+export default Benefit;
